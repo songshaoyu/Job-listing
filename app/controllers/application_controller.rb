657921f7ac6494cf639d231887c9a19aea34f257
songshaoyu <ahu8756@gmail.com>
2017-04-09 22:21:59 +0800

    debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  skip_before_action :verify_authenticity_token
   def require_is_admin
-     unless current_user.admin?
+     unless current_user.try(:admin?)
       flash[:alert] = "You are not admin"
       redirect_to root_path
     end
